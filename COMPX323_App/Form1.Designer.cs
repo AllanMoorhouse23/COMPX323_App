8fe9ddee3464b55f010649a11658f0ee934b1f23
Johnny <jknz84@gmail.com>
2019-05-20 12:38:11 +1200

    added test connection to form1 for mongoDB

--- COMPX323_App/Form1.Designer.cs
+++ COMPX323_App/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.openLogin = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -51,11 +52,22 @@
             this.openLogin.UseVisualStyleBackColor = true;
             this.openLogin.Click += new System.EventHandler(this.openLogin_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(505, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "MongoDB Connection";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.openLogin);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -69,6 +81,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button openLogin;
+        private System.Windows.Forms.Button button1;
     }
 }
 
